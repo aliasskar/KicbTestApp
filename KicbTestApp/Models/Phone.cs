e49da70c0ba15ae5670187875c39bce4108a06e9
aliasskar <aliaskarabdyrov@gmail.com>
2026-04-25 09:44:55 +0600

    Добавлены модели User и Phone с валидацией

---
+++ KicbTestApp/Models/Phone.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace KicbTestApp.Models
+{
+    public class Phone
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Номер телефона обязателен")]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        public int UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; } 
+    }
+}
---
+++ KicbTestApp/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+
+namespace KicbTestApp.Models
+{
+    public class User
+    {
+        [Key] 
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Имя обязательно")] 
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Некорректный Email")]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+        public List<Phone> Phones { get; set; } = new List<Phone>();
+    }
+}
